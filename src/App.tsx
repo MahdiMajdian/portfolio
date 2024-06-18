c99af9c3648743df04408adf1821745cd2cc8efc
Mahdi Majdian <MajdianMahdi@gmail.com>
2024-06-18 08:23:42 +0330

    setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import { Layout } from '@components';
-
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+const spring = {
+	type: 'spring',
+	stiffness: 700,
+	damping: 30,
+};
 function App() {
-	return <Layout>Hello React!</Layout>;
+	return (
+		<Layout>
+			<div className='mx-auto w-full mt-96'>
+				<motion.div layout transition={spring}></motion.div>
+			</div>
+		</Layout>
+	);
 }
 
 export default App;
