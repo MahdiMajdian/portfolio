be97fcf53a9b8d695bcc975e8071be12a0f59c5d
Mahdi Majdian <MajdianMahdi@gmail.com>
2024-06-22 22:14:38 +0330

    add icons and logo

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,5 @@
 export { default as Setting } from './setting.svg?react';
 export { default as Sort } from './sort.svg?react';
+export { default as Home } from './home.svg?react';
+export { default as Asterisk } from './asterisk.svg?react';
+export { default as Sparkle } from './sparkle.svg?react';
